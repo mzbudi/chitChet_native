8911e2975f9bb9d2be766c66d9fe1626db763fbc
MzBudi <ronibudianto361@gmail.com>
2020-02-20 21:33:36 +0700

    First Commit

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset', 'react-native-dotenv'],
+  sourceMaps: true,
+  plugins: [
+    '@babel/transform-flow-strip-types',
+    '@babel/proposal-class-properties',
+    '@babel/proposal-object-rest-spread',
+    '@babel/transform-runtime',
+  ],
+};
