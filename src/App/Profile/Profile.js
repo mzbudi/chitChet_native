d1a0022b6bbb0c1a601258a7714239d0d57ac3eb
MzBudi <ronibudianto361@gmail.com>
2020-02-23 15:59:35 +0700

    first success upload image

--- src/App/Profile/Profile.js
+++ src/App/Profile/Profile.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { ListItem, Icon } from 'react-native-elements';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, ToastAndroid } from 'react-native';
 import { connect } from 'react-redux';
 import { appFirebase } from '../../config/firebase';
+import ImagePicker from 'react-native-image-picker';
 
 class Profile extends Component {
+  state = {
+    photo: null,
+    blobPhoto: null
+  };
+
   handleLogout = () => {
     appFirebase
       .auth()
@@ -16,25 +22,100 @@ class Profile extends Component {
         console.log('somethings happens');
       });
   };
+
+  handlePicture = () => {
+    const options = {
+      storageOptions: {
+        quality: 0.8,
+        skipBackup: true,
+        path: 'images'
+      }
+    };
+    ImagePicker.showImagePicker(options, response => {
+      if (response.didCancel) {
+        ToastAndroid.show('Take Image Cancelled', ToastAndroid.SHORT);
+      } else if (response.error) {
+        ToastAndroid.show('Take Image Error', ToastAndroid.SHORT);
+      } else if (response.customButton) {
+        ToastAndroid.show('Take Image Cancelled', ToastAndroid.SHORT);
+      } else {
+        const source = response;
+
+        if (source) {
+          const ext = source.uri.split('.').pop(); // Extract image extension
+          const filename = `${this.props.auth.data.uid}.${ext}`;
+          this.setState({
+            photo: source
+          });
+          this.handleUpload(source.uri, filename)
+            .then(() => {
+              ToastAndroid.show('Success Upload Image', ToastAndroid.SHORT);
+            })
+            .catch(error => {
+              ToastAndroid.show(error, ToastAndroid.SHORT);
+            });
+        }
+      }
+    });
+  };
+
+  handleUpload = async (uri, image_name) => {
+    const response = await fetch(uri);
+    const blob = await response.blob();
+    const ref = appFirebase
+      .storage()
+      .ref()
+      .child(`picture/${image_name}`);
+
+    return ref.put(blob);
+  };
+
   render() {
     return (
       <View style={styles.container}>
+        {this.state.photo !== null ? (
+          <ListItem
+            containerStyle={styles.whiteColor}
+            key={1}
+            leftAvatar={{
+              size: 'large',
+              showEditButton: true,
+              source: {
+                uri: this.state.photo.uri
+              }
+            }}
+            title={'Budi'}
+            subtitle={'Si bgst'}
+            bottomDivider
+            onPress={() => {
+              this.handlePicture();
+            }}
+          />
+        ) : (
+          <ListItem
+            containerStyle={styles.whiteColor}
+            key={1}
+            leftAvatar={{
+              size: 'large',
+              showEditButton: true,
+              source: {
+                uri:
+                  'https://www.indomeme.id/wp-content/uploads/2020/01/polos.jpg'
+              }
+            }}
+            title={'Budi'}
+            subtitle={'Si bgst'}
+            bottomDivider
+            onPress={() => {
+              this.handlePicture();
+            }}
+          />
+        )}
+
         <ListItem
-          containerStyle={styles.whiteColor}
-          key={1}
-          leftAvatar={{
-            size: 'large',
-            showEditButton: true,
-            source: {
-              uri:
-                'https://www.indomeme.id/wp-content/uploads/2020/01/polos.jpg'
-            }
+          onPress={() => {
+            this.handleUpload();
           }}
-          title={'Budi'}
-          subtitle={'Si bgst'}
-          bottomDivider
-        />
-        <ListItem
           containerStyle={styles.whiteColor}
           title={<Text style={styles.midText}>Change Name</Text>}
           leftIcon={<Icon name="edit" type="feather" color="#517fa4" />}
@@ -80,6 +161,8 @@ const styles = StyleSheet.create({
 });
 
 const mapStateToProps = state => {
-  return {};
+  return {
+    auth: state.auth
+  };
 };
 export default connect(mapStateToProps)(Profile);
--- src/Loading/Loading.js
+++ src/Loading/Loading.js
@@ -1,10 +1,14 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { View, Text, ActivityIndicator, StyleSheet } from 'react-native';
 import { appFirebase } from '../config/firebase';
+import { connect } from 'react-redux';
 
 class Loading extends Component {
   componentDidMount() {
+    const { loginRequest } = this.props;
     appFirebase.auth().onAuthStateChanged(user => {
+      console.log(user);
+      loginRequest(user);
       this.props.navigation.navigate(user ? 'Home' : 'Login');
     });
   }
@@ -26,4 +30,16 @@ const styles = StyleSheet.create({
   }
 });
 
-export default Loading;
+const mapStateToProps = state => {
+  return {
+    auth: state.auth
+  };
+};
+const mapDispatchToProps = dispatch => ({
+  loginRequest: payload =>
+    dispatch({ payload, type: 'LOGIN_REQUEST_FULFILLED' })
+});
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Loading);
